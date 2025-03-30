5f702074acfe1977c940afcdd9dd7a2ceeb7c6ef
Blaze Greenhalgh <blazegreenhalgh1943@gmail.com>
2025-03-30 17:36:31 +0800

    Create full functionality for scoring (Question 6 Answer)

--- wordle.py
+++ wordle.py
@@ -1,12 +1,24 @@
 # Blaze Greenhalgh, 20043918, 30/03/2025
 def score_guess(guess, target):
     score = [0] * len(target)
-    if guess == target:
-        score = [2] * len(target)
-        return score
-    return score
+    guess_letters = list(guess)
+    target_letters = list(target)
 
-guess = "world"
-target = "hello"
+    # Finding correct pos chars first
+    for guess_index, guess_letter in enumerate(guess_letters):
+        for target_index, target_letter in enumerate(target_letters):
+            if guess_letter == target_letter and guess_index == target_index:
+                score[guess_index] = 2
+                for i in range(len(guess_letters)):
+                    if guess_letter in guess_letters[i]:
+                        guess_letters[i] = " " # Replace duplicates with incorrect value after correct pos is found
 
-print(score_guess(guess, target))
+    # Finding incorrect pos chars after finding correct pos
+    for guess_index, guess_letter in enumerate(guess_letters):
+        for target_index, target_letter in enumerate(target_letters):
+            if guess_letter == target_letter and score[guess_index] != 2:
+                score[guess_index] = 1
+
+
+
+
