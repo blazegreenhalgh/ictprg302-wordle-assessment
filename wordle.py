ba139c327c8e6e141929cbcb1a27d8b57b300dcc
Blaze Greenhalgh <blazegreenhalgh1943@gmail.com>
2025-03-30 17:30:19 +0800

    score_guess function created (Question 2 Answer)

--- wordle.py
+++ wordle.py
@@ -5,13 +5,3 @@ def score_guess(guess, target):
         score = [2] * len(target)
         return score
     return score
-
-    # score = []
-    # for letter in target:
-    #     score.append(0)
-
-guess_word = "world"
-target_word = "hello"
-
-print(score_guess(guess_word, target_word))
-
