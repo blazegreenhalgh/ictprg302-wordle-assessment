e67d3b35ab936aefc0a4435f5c9afb95b12712be
blazegreenhalgh <160311594+blazegreenhalgh@users.noreply.github.com>
2025-03-31 08:54:51 +0800

    Rearranged comments

--- wordle.py
+++ wordle.py
@@ -9,9 +9,10 @@ def score_guess(guess, target):
         for target_index, target_letter in enumerate(target_letters):
             if guess_letter == target_letter and guess_index == target_index:
                 score[guess_index] = 2
+                # Replace duplicates with incorrect value after correct pos is found
                 for i in range(len(guess_letters)):
                     if guess_letter in guess_letters[i]:
-                        guess_letters[i] = " " # Replace duplicates with incorrect value after correct pos is found
+                        guess_letters[i] = " " 
 
     # Finding incorrect pos chars after finding correct pos
     for guess_index, guess_letter in enumerate(guess_letters):
