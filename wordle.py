9d06d83d279ede9af05e8a22178c2299a4602812
Blaze Greenhalgh <blazegreenhalgh1943@gmail.com>
2025-04-02 20:26:59 +0800

    Create format function, guess function, help function

--- wordle.py
+++ wordle.py
@@ -1,3 +1,11 @@
+def help_message():
+    attempts = 6
+    print("<< Welcome to CLI Wordle! >>")
+    print("You have 6 attempts to guess the secret word!")
+
+
+import random
+
 with open("sources/all_words.txt") as file:
     all_words = file.read().splitlines()
     for word in all_words:
@@ -8,17 +16,15 @@ with open("sources/target_words.txt") as file:
     for target_word in target_words:
         target_word.strip()
 
-print(all_words[0:5])
-print(target_words[0:5])
+target_word = random.choice(target_words)
+score = [0] * len(target_word)
 
-# Blaze Greenhalgh, 20043918, 30/03/2025
+
 def score_guess(guess, target):
     """ This functions takes two strings (guess, target) and determines if each character from one matches in value and position with the other.
     1 = Value match, 2 = Value and Position match. A list is returned with items equal to the length of the target string."""
-    score = [0] * len(target)
-    guess_letters = list(guess)
-    target_letters = list(target)
-
+    guess_letters = list(guess.lower())
+    target_letters = list(target.lower())
     # Finding correct pos chars first
     for guess_index, guess_letter in enumerate(guess_letters):
         if guess_letter == target_letters[guess_index]:
@@ -29,7 +35,7 @@ def score_guess(guess, target):
 
 
     # Finding incorrect pos chars after finding correct pos
-    for guess_index, guess_letter in enumerate(guess_letters): 
+    for guess_index, guess_letter in enumerate(guess_letters):
         if guess_letter in target_letters and guess_letter is not None:
             score[guess_index] = 1
             # Replace duplicates with incorrect value after correct pos is found
@@ -38,11 +44,49 @@ def score_guess(guess, target):
 
     return score
 
-# # Arrange
-# guess = "world"
-# target = "hello"
+def format_score(score, guess):
+    yellow = {"start": "\33[43m", "end": "\33[0m"}
+    green = {"start": '\x1b[6;30;42m', "end": '\x1b[0m'}
+    formatted_guess = list(guess)
+    for i, letter in enumerate(formatted_guess):
+        if score[i] == 1:
+            formatted_guess[i] = f"{yellow["start"]} {letter} {yellow["end"]}"
+        elif score[i] == 2:
+            formatted_guess[i] = f"{green["start"]} {letter} {green["end"]}"
+        else:
+            formatted_guess[i] = f" {letter} "
+    return formatted_guess
+
+
+
+
+
+
+def guess():
+    while True:
+        attempts = 6
+        print(f"{attempts} Attempts remaining...")
+        print("What is your 5-letter guess?")
+        input_guess = input("> ").lower()
+        if len(input_guess) < 5:
+            print("Too short! Must be 5 letters!")
+            continue
+        if len(input_guess) > 5:
+            print("Too long! Must be 5 letters!")
+            continue
+        score = score_guess(input_guess, target_word)
+        print(*format_score(score, input_guess))
+
+help_message()
+guess()
+
+
+
+
+# guess = "bca"
+# target = "bba"
 #
-# # Act
-# print(score_guess(guess, target))
+# print("FINAL SCORE: ", score_guess(guess, target))
+
 
 
