89898cefa310dd692df85a7aec711a8e62638ac6
Blaze Greenhalgh <blazegreenhalgh1943@gmail.com>
2025-04-02 19:07:49 +0800

    Added random word grabber (Answers for Questions 9 & 10)

--- wordle.py
+++ wordle.py
@@ -1,5 +1,20 @@
+with open("sources/all_words.txt") as file:
+    all_words = file.read().splitlines()
+    for word in all_words:
+        word.strip()
+
+with open("sources/target_words.txt") as file:
+    target_words = file.read().splitlines()
+    for target_word in target_words:
+        target_word.strip()
+
+print(all_words[0:5])
+print(target_words[0:5])
+
 # Blaze Greenhalgh, 20043918, 30/03/2025
 def score_guess(guess, target):
+    """ This functions takes two strings (guess, target) and determines if each character from one matches in value and position with the other.
+    1 = Value match, 2 = Value and Position match. A list is returned with items equal to the length of the target string."""
     score = [0] * len(target)
     guess_letters = list(guess)
     target_letters = list(target)
@@ -23,7 +38,11 @@ def score_guess(guess, target):
 
     return score
 
-
-
+# # Arrange
+# guess = "world"
+# target = "hello"
+#
+# # Act
+# print(score_guess(guess, target))
 
 
