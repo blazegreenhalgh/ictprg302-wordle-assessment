063a0187a5701afb3416150787a2d3a796f56fe6
blazegreenhalgh <160311594+blazegreenhalgh@users.noreply.github.com>
2025-04-02 14:29:44 +0800

    Bug with duplicate letters extra fixed

--- wordle.py
+++ wordle.py
@@ -6,19 +6,20 @@ def score_guess(guess, target):
 
     # Finding correct pos chars first
     for guess_index, guess_letter in enumerate(guess_letters):
-        if guess_letter in target_letters[guess_index]:
+        if guess_letter == target_letters[guess_index]:
             score[guess_index] = 2
             # Replace duplicates with incorrect value after correct pos is found
             target_letters[guess_index] = None
+            guess_letters[guess_index] = None
+
 
     # Finding incorrect pos chars after finding correct pos
-    for guess_index, guess_letter in enumerate(guess_letters):
-        if guess_letter in target_letters:
+    for guess_index, guess_letter in enumerate(guess_letters): 
+        if guess_letter in target_letters and guess_letter is not None:
             score[guess_index] = 1
             # Replace duplicates with incorrect value after correct pos is found
             target_index = target_letters.index(guess_letter)
             target_letters[target_index] = None
-        
 
     return score
 
