5879cc09018a21a694b2f14fc9bee73cfd5d2259
Blaze Greenhalgh <blazegreenhalgh1943@gmail.com>
2025-04-03 19:31:59 +0800

    feat: add fail/win functionality, cleanup code

--- wordle.py
+++ wordle.py
@@ -1,11 +1,3 @@
-def help_message():
-    attempts = 6
-    print("\n--------------------")
-    print("Welcome to CLI Wordle!")
-    print("You have 6 attempts to guess the secret word!")
-    print("--------------------\n")
-
-
 import random
 
 with open("sources/all_words.txt") as file:
@@ -15,14 +7,14 @@ with open("sources/all_words.txt") as file:
 
 with open("sources/target_words.txt") as file:
     target_words = file.read().splitlines()
-    for target_word in target_words:
-        target_word.strip()
+    for word in target_words:
+        word.strip()
 
 
 def score_guess(guess, target):
     """ This functions takes two strings (guess, target) and determines if each character from one matches in value and position with the other.
     1 = Value match, 2 = Value and Position match. A list is returned with items equal to the length of the target string."""
-    score = [0] * len(target_word)
+    score = [0] * len(target)
     guess_letters = list(guess.lower())
     target_letters = list(target.lower())
     # Finding correct pos chars first
@@ -32,8 +24,6 @@ def score_guess(guess, target):
             # Replace duplicates with incorrect value after correct pos is found
             target_letters[guess_index] = None
             guess_letters[guess_index] = None
-
-
     # Finding incorrect pos chars after finding correct pos
     for guess_index, guess_letter in enumerate(guess_letters):
         if guess_letter in target_letters and guess_letter is not None:
@@ -41,7 +31,6 @@ def score_guess(guess, target):
             # Replace duplicates with incorrect value after correct pos is found
             target_index = target_letters.index(guess_letter)
             target_letters[target_index] = None
-
     return score
 
 def format_score(score, guess):
@@ -59,14 +48,19 @@ def format_score(score, guess):
 
 
 def play():
+    print("\n--------------------")
+    print("Welcome to CLI Wordle!")
+    print("You have 6 attempts to guess the secret word!")
+    print("--------------------\n")
     target_word = random.choice(target_words)
-    attempts = 1
-    print(target_word)
-
+    attempts = 6
     print("What is your 5-letter guess?")
     while True:
         print(f"{attempts} Attempts remaining...")
         input_guess = input("> ").lower()
+        if input_guess == "help":
+            help_message(target_word)
+            continue
         if len(input_guess) < 5:
             print("Too short! Must be 5 letters!")
             continue
@@ -74,19 +68,30 @@ def play():
             print("Too long! Must be 5 letters!")
             continue
         score = score_guess(input_guess, target_word)
-        print(score)
         print(*format_score(score, input_guess))
+        if score == [2, 2, 2, 2, 2]:
+            print("You guessed the target word!")
+            end_game()
+            break
+
         attempts -= 1
         if attempts == 0:
+            print("\n--------------------")
+            print("You lost!")
+            print(f"The word was \33[43m {target_word} \33[0m")
+            print("--------------------\n")
             end_game()
             break
 
 
+def help_message(target):
+    print("\n--------------------")
+    print(f"I'll give you a hint: {target}")
+    print("--------------------\n")
 
 
 
 def end_game():
-    print("You lost!")
     print("Would you like to play again?")
     input_restart = input("> ").strip().lower()
     if input_restart[0] == 'y':
@@ -97,7 +102,6 @@ def end_game():
 
 
 
-help_message()
 play()
 
 
